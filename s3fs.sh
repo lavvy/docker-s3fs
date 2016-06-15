3f2277a01a1d4c1a8c3022f1b260072292faa5ff
lavvy <lavashonline@gmail.com>
2016-06-15 11:19:20 -0700

    added sudo

--- s3fs.sh
+++ s3fs.sh
@@ -3,4 +3,5 @@
 echo ${AWSS3User}:${AWSS3Secret} > /etc/passwd-s3fs
 chmod 600 /etc/passwd-s3fs
 mkdir -p ${MOUNTPOINT}
-s3fs ${BUCKETNAME} ${MOUNTPOINT}  -o passwd_file=/etc/passwd-s3fs
+chmod 777 ${MOUNTPOINT}
+sudo s3fs -o allow_other ${BUCKETNAME} ${MOUNTPOINT}
